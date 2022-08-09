47223a017f6c23122874ce7a149fa5224325ae23
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-09 21:05:07 +0100

    Initial commit

---
+++ src/main/java/dev/webfx/lib/circlespacker/CirclesPacker.java
@@ -0,0 +1,20 @@
+package dev.webfx.lib.circlespacker;
+
+/**
+ * @author Bruno Salmon
+ */
+interface CirclesPacker {
+
+    void setContainerSize(double width, double height);
+
+    void setCirclesCount(int count);
+
+    boolean hasChanged();
+
+    double getCirclesRadius();
+
+    double getCircleCenterX(int index);
+
+    double getCircleCenterY(int index);
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.lib.circlespacker {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.base;
+    requires javafx.graphics;
+
+    // Exported packages
+    exports dev.webfx.lib.circlespacker;
+
+}
